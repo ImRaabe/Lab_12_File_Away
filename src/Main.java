e57f443700eff24e7df301b1065988a4f8841635
ImRaabe <raabees@mail.uc.edu>
2024-04-21 13:11:18 -0400

    Lab12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import javax.swing.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.nio.file.Path;
+import javax.swing.JFileChooser;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFileChooser chooser = new JFileChooser();
+        int value = chooser.showOpenDialog(null);
+
+        if (value == JFileChooser.APPROVE_OPTION) {
+            File selectedFile = chooser.getSelectedFile();
+            String fileName = selectedFile.getName();
+            int lineCount = 0;
+            int wordCount = 0;
+            int charCount = 0;
+
+            try {
+                Scanner inFile = new Scanner(selectedFile);
+                while (inFile.hasNextLine()) {
+                    String line = inFile.nextLine();
+                    lineCount++;
+                    charCount += line.length();
+                    String[] words = line.split("\\s+");
+                    wordCount += words.length;
+                }
+                inFile.close();
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Summary Report:");
+            System.out.println("File Name: " + fileName);
+            System.out.println("Number of Lines: " + lineCount);
+            System.out.println("Number of Words: " + wordCount);
+            System.out.println("Number of Characters: " + charCount);
+        } else {
+            System.out.println("No file selected.");
+        }
     }
 }
